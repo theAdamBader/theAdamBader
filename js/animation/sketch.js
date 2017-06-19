e1809ffbd165b0c54be1d32fc4de646d7fe53905
Adam Bader <abade011@gold.ac.uk>
2017-06-19 22:58:28 +0100

    workInProgress

--- js/animation/sketch.js
+++ js/animation/sketch.js
@@ -1,9 +1,11 @@
 var ranger1; //creating an object to be called
+var x = 550;
+var y = 200;
 
 function setup() {
-	var newCanvas = createCanvas(64, 64);
+	var newCanvas = createCanvas(displayWidth, displayHeight);
 	newCanvas.parent('p5js');
-	//newCanvas.position(0, 0);
+	//newCanvas.position(x, y);
 	ranger1 = new Sprites();
 	ranger1.set();
 }
--- js/animation/sprite.js
+++ js/animation/sprite.js
@@ -1,21 +1,28 @@
 function Sprites(){
 
 var ranger = new Array(3);
+var ranger1 = new Array(2);
 var context = canvas.getContext("2d");
 var currentImage = 0;
 var frameStartTime = 0;
-var x = 0;
-var y = 32;
+var x = 32;
+var x1 = 382;
+var y = 140;
 
 Sprites.prototype.set = function(){
 	for(var i = 0; i < ranger.length; i++){
 	    ranger[i] = loadImage("./img/assets/ranger/attack"+i+".png");
 	}
+  for(var i = 0; i < ranger1.length; i++){
+      ranger1[i] = loadImage("./img/assets/ranger/idle"+i+".png");
+  }
 }
 
 Sprites.prototype.display = function() {
-    context.clearRect(x, y, 32, 32);
+    context.clearRect(x, y, displayWidth, displayHeight);
+    //context.clearRect(x + 350, y, 32, 32);
     image(ranger[currentImage], x, y);
+    image(ranger1[currentImage], x1, y);
   }
 
 Sprites.prototype.update = function() {
@@ -27,7 +34,7 @@ Sprites.prototype.update = function() {
     
       currentImage ++;//increments the image by one every frame
       
-      if (currentImage >= ranger.length)// when it reachs the end of the array, it goes back to 0
+      if (currentImage >= ranger.length || currentImage >= ranger1.length )// when it reachs the end of the array, it goes back to 0
       {
         currentImage = 0;
       }
