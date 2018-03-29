523a975e79cdb95d826c921dea8dcb8dbcf79539
Adam Bader <abade011@gold.ac.uk>
2018-03-29 23:49:39 +0100

    moved position x

--- js/animation/spriteName.js
+++ js/animation/spriteName.js
@@ -4,7 +4,7 @@ var names = new Array(39);
 var context = canvas.getContext("2d");
 var currentImage = 0;
 var frameStartTime = 0;
-var x = 0;
+var x = 50;
 var y = 0;
 
 Sprite.prototype.set = function(){
